8ac8951a7b0d888ca8f6bc6d00abc6ac7d7f4fa9
ABDALLAH-XX <abdallahhafidhou@hotmail.com>
2026-08-16 00:37:30 +0200

    Random Messages:
    use randomSeed(analogRead(0)); and random(5) to print

--- ESP32_Button/src/main.cpp
+++ ESP32_Button/src/main.cpp
@@ -2,23 +2,23 @@
 
 #define BUTTON_PIN 26
 
-int pressCount = 0;
 int lastState = HIGH;
+const char* messages[] = {"Hello!", "Awesome!", "Keep going!", "ESP32 rocks!",
+"Great job!"};
+
 
 void setup() {
   Serial.begin(9600);
   pinMode(BUTTON_PIN, INPUT_PULLUP);
-  Serial.println("Button Counter Started");
+  randomSeed(analogRead(0));
 }
 
 void loop() {
   int currentState = digitalRead(BUTTON_PIN);
   
   if (lastState == HIGH && currentState == LOW) {
-    pressCount++;
-    Serial.print("Press Button: ");
-    Serial.print(pressCount);
-    Serial.println(" - Button PRESSED!");
+    int index = random(5);
+    Serial.println(messages[index]);
   } 
   
   lastState = currentState;
