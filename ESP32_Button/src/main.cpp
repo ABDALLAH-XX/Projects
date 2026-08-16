3414d3cbde258de5c81602f81d11cc16f4ff65c2
ABDALLAH-XX <abdallahhafidhou@hotmail.com>
2026-08-17 00:42:44 +0200

    Short press:
    This program mesures the amount of time in which the button is pressed.
    
    If it's under 500ms a message is displayed on the terminal

--- ESP32_Button/src/main.cpp
+++ ESP32_Button/src/main.cpp
@@ -1,38 +1,34 @@
 #include <Arduino.h>
 
-#define BUTTON_PIN 26
+#define BUTTON_PIN 26        // GPIO26 pin connected to button
+#define SHORT_PRESS_TIME 500 // 500 milliseconds
 
-int lastState = HIGH;
-int currentState;
-unsigned long lastDebounceTime = 0;
-const unsigned long debounceDelay = 50; // 50ms debounce
-
+// Variables will change:
+int lastState = LOW;  // the previous state from the input pin
+int currentState;     // the current reading from the input pin
+unsigned long pressedTime = 0;
+unsigned long releasedTime = 0;
 
 void setup() {
   Serial.begin(9600);
   pinMode(BUTTON_PIN, INPUT_PULLUP);
-  Serial.println("=== Button with Debouncing ===");
-  Serial.println("Press the button - no false triggers!");
 }
 
 void loop() {
-  int reading = digitalRead(BUTTON_PIN);
-  
-  // Check if state changed
-  if (reading != lastState) {
-    lastDebounceTime = millis();
-  }
-  
-  // Wait for debounce time
-  if ((millis() - lastDebounceTime) > debounceDelay) {
-    if (reading != currentState) {
-      currentState = reading;
+  // read the state of the switch/button
+  currentState = digitalRead(BUTTON_PIN);
 
-      if (currentState == LOW) {
-        Serial.println("Button PRESSED (debounced)!");
-      }
-    }
+  if (lastState == HIGH && currentState == LOW)
+    pressedTime = millis();
+  else if (lastState == LOW && currentState == HIGH) {
+    releasedTime = millis();
+
+    long pressDuration = releasedTime - pressedTime;
+
+    if (pressDuration < SHORT_PRESS_TIME)
+      Serial.println("A short press is detected");
   }
-  
-  lastState = reading;
+
+  // save the last state
+  lastState = currentState;
 }
