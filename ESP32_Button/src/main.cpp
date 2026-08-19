3bbd07fd4b31c37ea9069761f233a678110fd03a
ABDALLAH-XX <abdallahhafidhou@hotmail.com>
2026-08-19 21:41:21 +0200

    Short press and long press with debouncing after released:
    ezButton library

--- ESP32_Button/src/main.cpp
+++ ESP32_Button/src/main.cpp
@@ -1,38 +1,38 @@
 #include <Arduino.h>
+#include <ezButton.h>
 
-#define BUTTON_PIN 26        // GPIO26 pin connected to button
+
+//#define BUTTON_PIN 26        // GPIO26 pin connected to button
 #define SHORT_PRESS_TIME 1000 // 1000 milliseconds
 #define LONG_PRESS_TIME 1000 // 1000 milliseconds
 
+ezButton button(26); // create ezButton object that allows that attach to pin GPIO26
+
 // Variables will change:
-int lastState = LOW;  // the previous state from the input pin
-int currentState;     // the current reading from the input pin
 unsigned long pressedTime = 0;
 unsigned long releasedTime = 0;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  button.setDebounceTime(50); // set debounce time to 50s
 }
 
 void loop() {
   // read the state of the switch/button
-  currentState = digitalRead(BUTTON_PIN);
+  button.loop(); // MUST call the loop() function first
 
-  if (lastState == HIGH && currentState == LOW) {
+  if (button.isPressed())
     pressedTime = millis();
-  } else if (lastState == LOW && currentState == HIGH) {
+
+  if (button.isReleased()) {
     releasedTime = millis();
 
     long pressDuration = releasedTime - pressedTime;
 
     if (pressDuration < SHORT_PRESS_TIME)
       Serial.println("A short press is detected");
-    
+
     if (pressDuration > LONG_PRESS_TIME)
       Serial.println("A long press is detected");
   }
-
-  // save the last state
-  lastState = currentState;
 }
