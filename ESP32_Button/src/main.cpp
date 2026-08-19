5e88090fb295eb1a2ed349629e19edb776c69219
ABDALLAH-XX <abdallahhafidhou@hotmail.com>
2026-08-19 20:16:45 +0200

    Short press and long press after released:
    first if:
    check if button is press and start recording press time
    
    else if:
    record releaded time and substract with the press time and evaluate it it's a short or a long press
    
    update with the current state

--- ESP32_Button/src/main.cpp
+++ ESP32_Button/src/main.cpp
@@ -1,14 +1,14 @@
 #include <Arduino.h>
 
 #define BUTTON_PIN 26        // GPIO26 pin connected to button
+#define SHORT_PRESS_TIME 1000 // 1000 milliseconds
 #define LONG_PRESS_TIME 1000 // 1000 milliseconds
 
 // Variables will change:
 int lastState = LOW;  // the previous state from the input pin
 int currentState;     // the current reading from the input pin
 unsigned long pressedTime = 0;
-bool isPressing = false;
-bool isLongDetected = false;
+unsigned long releasedTime = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -21,19 +21,16 @@ void loop() {
 
   if (lastState == HIGH && currentState == LOW) {
     pressedTime = millis();
-    isPressing = true;
-    isLongDetected = false;
   } else if (lastState == LOW && currentState == HIGH) {
-    isPressing = false;
-  }
-  
-  if (isPressing == true && isLongDetected == false){
-    long pressDuration = millis() - pressedTime;
+    releasedTime = millis();
 
-    if (pressDuration > LONG_PRESS_TIME){
+    long pressDuration = releasedTime - pressedTime;
+
+    if (pressDuration < SHORT_PRESS_TIME)
+      Serial.println("A short press is detected");
+    
+    if (pressDuration > LONG_PRESS_TIME)
       Serial.println("A long press is detected");
-      isLongDetected = true;
-    }
   }
 
   // save the last state
