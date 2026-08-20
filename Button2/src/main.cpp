f64e8e82dc709b8e9ac244507dc1df63ac4b5211
ABDALLAH-XX <abdallahhafidhou@hotmail.com>
2026-08-20 22:30:51 +0200

    2 Buttons using debounce and short delay

--- Button2/src/main.cpp
+++ Button2/src/main.cpp
@@ -1,26 +1,41 @@
 #include <Arduino.h>
 
 #define RED_BUTTON 27
-#define BLUE_BUTTON 26
+#define BLUE_BUTTON 25
 
+int lastState1 = HIGH;
+int lastState2 = HIGH;
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200);
   // initialize the pushbutton as an pull-up input
   pinMode(RED_BUTTON, INPUT_PULLUP);
   pinMode(BLUE_BUTTON, INPUT_PULLUP);
+  Serial.begin(115200);
 }
 
 void loop() {
-  if (digitalRead(RED_BUTTON) == LOW) {
-    Serial.println("Bouton rouge pressé !");
-    delay(200);
+  int currentState1 = digitalRead(RED_BUTTON);
+  int currentState2 = digitalRead(BLUE_BUTTON);
+
+
+  if (currentState1 != lastState1) {
+    if (currentState1 == LOW) {
+      Serial.println("Bouton rouge presse !");
+    } else {
+      Serial.println("Bouton rouge relache !");
+    }
+    lastState1 = currentState1;
   }
 
-  if (digitalRead(BLUE_BUTTON) == LOW) {
-    Serial.println("Bouton bleu pressé !");
-    delay(200);
- }
-
+  if (currentState2 != lastState2) {
+    if (currentState2 == LOW) {
+      Serial.println("Bouton bleu presse !");
+    } else {
+      Serial.println("Bouton bleu relache !");
+    }
+    lastState2 = currentState2;
+  }
+  
+  delay(10);
 }
