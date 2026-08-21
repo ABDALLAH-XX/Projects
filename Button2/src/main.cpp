208f52dfa9bdfd4ec3075d231428a5fe0fb1abde
ABDALLAH-XX <abdallahhafidhou@hotmail.com>
2026-08-21 22:55:11 +0200

    Multiple buttons with debounce using ezButton

--- Button2/src/main.cpp
+++ Button2/src/main.cpp
@@ -1,41 +1,39 @@
 #include <Arduino.h>
+#include <ezButton.h>
+
 
 #define RED_BUTTON 27
 #define BLUE_BUTTON 25
 
-int lastState1 = HIGH;
-int lastState2 = HIGH;
+ezButton button1(RED_BUTTON);  // create ez Button object
+ezButton button2(BLUE_BUTTON);  // create ez Button object
+
 
 void setup() {
   // put your setup code here, to run once:
   // initialize the pushbutton as an pull-up input
-  pinMode(RED_BUTTON, INPUT_PULLUP);
-  pinMode(BLUE_BUTTON, INPUT_PULLUP);
-  Serial.begin(115200);
+  button1.setDebounceTime(100); // 100ms
+  button2.setDebounceTime(100); // 100ms
+  Serial.begin(9600);
 }
 
 void loop() {
-  int currentState1 = digitalRead(RED_BUTTON);
-  int currentState2 = digitalRead(BLUE_BUTTON);
+  button1.loop(); // must call loop() function first
+  button2.loop();
 
+  // get button state after debounce
+  int button1State = button1.getState();
+  int button2State = button2.getState();
 
-  if (currentState1 != lastState1) {
-    if (currentState1 == LOW) {
-      Serial.println("Bouton rouge presse !");
-    } else {
-      Serial.println("Bouton rouge relache !");
-    }
-    lastState1 = currentState1;
-  }
+  if (button1.isPressed())
+    Serial.println("Button 1 is pressed");
 
-  if (currentState2 != lastState2) {
-    if (currentState2 == LOW) {
-      Serial.println("Bouton bleu presse !");
-    } else {
-      Serial.println("Bouton bleu relache !");
-    }
-    lastState2 = currentState2;
-  }
-  
-  delay(10);
+  if (button1.isReleased())
+    Serial.println("Button 1 is released");
+
+  if (button2.isPressed())
+    Serial.println("Button 2 is pressed");
+
+  if (button2.isReleased())
+    Serial.println("Button 2 is released");
 }
