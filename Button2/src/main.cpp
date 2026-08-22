e7e69f8d9eb6f19984319d6a87e21c803423f499
ABDALLAH-XX <abdallahhafidhou@hotmail.com>
2026-08-22 22:08:22 +0200

    Multiple buttons using array:
    Fixed buttons display problems by creating a common ground with the first button.

--- Button2/src/main.cpp
+++ Button2/src/main.cpp
@@ -1,39 +1,42 @@
 #include <Arduino.h>
 #include <ezButton.h>
 
+#define BUTTON_NUM 3 // the number of buttons
 
-#define RED_BUTTON 27
-#define BLUE_BUTTON 25
 
-ezButton button1(RED_BUTTON);  // create ez Button object
-ezButton button2(BLUE_BUTTON);  // create ez Button object
-
+ezButton buttonArray[] = {
+  ezButton(27),
+  ezButton(26),
+  ezButton(25)
+};
 
 void setup() {
   // put your setup code here, to run once:
-  // initialize the pushbutton as an pull-up input
-  button1.setDebounceTime(100); // 100ms
-  button2.setDebounceTime(100); // 100ms
-  Serial.begin(9600);
+  Serial.begin(115200);
+  
+  for (byte i = 0; i < BUTTON_NUM; i++) {
+    buttonArray[i].setDebounceTime(100); // 100ms
+  }
 }
 
 void loop() {
-  button1.loop(); // must call loop() function first
-  button2.loop();
+  for (byte i = 0; i < BUTTON_NUM; i++)
+    buttonArray[i].loop(); // MUST call the loop() function
 
-  // get button state after debounce
-  int button1State = button1.getState();
-  int button2State = button2.getState();
+  for (byte i = 0; i< BUTTON_NUM; i++) {
+    // get button state after debounce
+    int buttonState = buttonArray[i].getState();
 
-  if (button1.isPressed())
-    Serial.println("Button 1 is pressed");
+    if (buttonArray[i].isPressed()) {
+      Serial.println("The button ");
+      Serial.print(i + 1);
+      Serial.println(" is pressed");
+    }
 
-  if (button1.isReleased())
-    Serial.println("Button 1 is released");
-
-  if (button2.isPressed())
-    Serial.println("Button 2 is pressed");
-
-  if (button2.isReleased())
-    Serial.println("Button 2 is released");
+    if (buttonArray[i].isReleased()) {
+      Serial.println("The button ");
+      Serial.print(i + 1);
+      Serial.println(" is released");
+    }
+  }
 }
