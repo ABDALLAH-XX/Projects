c31f8d7fed0fad2f13d62cf0692f336ce6f1e719
ABDALLAH-XX <abdallahhafidhou@hotmail.com>
2026-08-24 14:29:21 +0200

    Button Buzzer :
    Simple Sound

---
+++ Button_Buzzer/src/main.cpp
@@ -0,0 +1,31 @@
+#include <Arduino.h>
+
+#define BUTTON_PIN 21
+#define BUZZER_PIN 18
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  pinMode(BUZZER_PIN, OUTPUT);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int buttonState = digitalRead(BUTTON_PIN); 
+
+  if (buttonState == LOW) {
+    Serial.println("The button is being pressed");
+    digitalWrite(BUZZER_PIN, HIGH); // turn on
+  }
+  else {
+    if (buttonState == HIGH) {
+      Serial.println("The button is released");
+      digitalWrite(BUZZER_PIN, LOW); // turn off
+    }
+  }
+}
